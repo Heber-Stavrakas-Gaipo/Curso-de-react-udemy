c183ea923909e077c6ca1f91c0899008358c74c2
Héber Stavrakas Gaipo <contatohebergaipo@gmail.com>
2023-09-02 17:00:51 -0300

    lifecycle methods

--- src/App.js
+++ src/App.js
@@ -3,40 +3,84 @@ import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.handlePClick = this.handlePClick.bind(this); //método necessário para usar a requisição this dentro das respectivas funções
-    this.handleSwitchName = this.handleSwitchName.bind(this);
-    // a função handleACLick não utiliza esses métodos pois é uma arrow function, que não possui this internamente, sendo assim obrigada a buscar no seu pai e caindo apenas no this.state
+  //   this.handlePClick = this.handlePClick.bind(this); //método necessário para usar a requisição this dentro das respectivas funções
+  //   this.handleSwitchName = this.handleSwitchName.bind(this);
+  //   // a função handleACLick não utiliza esses métodos pois é uma arrow function, que não possui this internamente, sendo assim obrigada a buscar no seu pai e caindo apenas no this.state
 
-    this.state = {
-      name: "Héber Stavrakas Gaipo" ,
-      counter: 1,
-    };
+  //   this.state = {
+  //     name: "Héber Stavrakas Gaipo" ,
+  //     counter: 1,
+  //   };
+  // }
+
+  // handlePClick() {
+  //   const {name} = this.state;
+  //   console.log(`<p> clicado e o nome é ${name}`)
+  // }
+
+  // handleSwitchName() {
+  //   this.setState({name: 'Heber'})
+  // }
+
+  // handleAClick = (event) => {
+  //   event.preventDefault(); //desabilitar a ação padrão da tag a que é o href
+  //   const {counter} = this.state
+  //   this.setState({counter: counter + 1})
+  // }
+
+  state = {
+    counter: 0,
+    posts: [
+      {
+        id: 1,
+        title: "Título 1",
+        body: "Corpo 1"
+      },
+      {
+        id: 2,
+        title: "Título 2",
+        body: "Corpo 2"
+      },
+      {
+        id: 3,
+        title: "Título 3",
+        body: "Corpo 3"
+      },
+    ]
+  };
+
+  timeoutUpdate = null;
+
+  componentDidMount() {
+    this.handleTimeOut();
   }
 
-  handlePClick() {
-    const {name} = this.state;
-    console.log(`<p> clicado e o nome é ${name}`)
+  componentDidUpdate() {
+    this.handleTimeOut()
   }
 
-  handleSwitchName() {
-    this.setState({name: 'Heber'})
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdate);
   }
 
-  handleAClick = (event) => {
-    event.preventDefault(); //desabilitar a ação padrão da tag a que é o href
-    const {counter} = this.state
-    this.setState({counter: counter + 1})
+  handleTimeOut = () => {
+    const {posts, counter} = this.state
+    posts[0].title = "O título mudou!"
+
+    setTimeout(() => {
+      this.setState({posts, counter: counter + 1});
+    }, 1000)
   }
 
   render() {
-    const {name, counter} = this.state;
+    const {posts, counter} = this.state;
 
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p onClick={this.handleSwitchName}>
             {name} {counter}
@@ -50,7 +94,14 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
+        <h1>{counter}</h1>
+        {posts.map(post => (
+          <div key={post.id}>
+            <h1>{post.title}</h1>
+          <p>{post.body}</p>
+          </div>
+        ))}
       </div>
     );
   }
