fc1e350ec29277b9a7a9e44974d844437527d047
Héber Stavrakas Gaipo <contatohebergaipo@gmail.com>
2023-09-04 21:46:17 -0300

    Migrando para React Hooks

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,14 @@
-import './styles.css'
-import { Component } from "react";
+import "./styles.css";
 
-export class Button extends Component {
-    render() {
-        const {text, onClick, disabled} = this.props;
-        return (<button disabled={disabled} className='morePosts' onClick={onClick}>{text}</button>)
-    }
-}
+export const Button = ({text, onClick, disabled}) => (
+  <button className="morePosts" onClick={onClick} disabled={disabled}>
+    {text}
+  </button>
+);
+
+// export class Button extends Component {
+//     render() {
+//         const {text, onClick, disabled} = this.props;
+//         return (<button disabled={disabled} className='morePosts' onClick={onClick}>{text}</button>)
+//     }
+// }
