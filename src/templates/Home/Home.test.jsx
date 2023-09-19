9938636ec262456d0e5cf5e767e37d5da235dc09
Héber Stavrakas Gaipo <contatohebergaipo@gmail.com>
2023-09-18 23:04:29 -0300

    Testando os componentes renderizados pelo template <Home />, testando a busca por posts, testando o botão 'Load more posts'

--- src/templates/Home/Home.test.jsx
+++ src/templates/Home/Home.test.jsx
@@ -3,6 +3,7 @@ import { setupServer } from 'msw/node';
 
 import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import { Home } from '.';
+import userEvent from '@testing-library/user-event';
 
 const handlers = [
   rest.get('*jsonplaceholder.typicode.com*', async (req, res, ctx) => {
@@ -29,6 +30,13 @@ const handlers = [
           body: 'body3',
           url: 'img3.jpg',
         },
+        {
+          userId: 4,
+          id: 4,
+          title: 'title4',
+          body: 'body4',
+          url: 'img4.jpg',
+        },
       ]),
     );
   }),
@@ -52,10 +60,67 @@ describe('<Home />', () => {
   it('should render search, posts and load more', async () => {
     render(<Home />);
 
+    expect.assertions(3);
+
     await waitForElementToBeRemoved(
       () => screen.queryByText('Sua busca não retornou resultados', { name: 'MyDialog' }),
       { timeout: 99999 },
     );
-    screen.debug();
+
+    const search = screen.getByPlaceholderText(/filtrar por título/i);
+    expect(search).toBeInTheDocument();
+
+    const images = screen.getAllByRole('img', { name: /title/i });
+    expect(images).toHaveLength(3);
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('should search for posts', async () => {
+    render(<Home />);
+
+    // expect.assertions(12);
+
+    await waitForElementToBeRemoved(
+      () => screen.queryByText('Sua busca não retornou resultados', { name: 'MyDialog' }),
+      { timeout: 99999 },
+    );
+
+    const search = screen.getByPlaceholderText(/filtrar por título/i);
+    expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title3' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title4' })).not.toBeInTheDocument();
+
+    userEvent.type(search, 'title4');
+    expect(screen.queryByRole('heading', { name: 'title1' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title2' })).not.toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title3' })).not.toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title4' })).toBeInTheDocument();
+
+    userEvent.clear(search);
+    expect(screen.getByRole('heading', { name: 'title1' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title2' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'title3' })).toBeInTheDocument();
+    expect(screen.queryByRole('heading', { name: 'title4' })).not.toBeInTheDocument();
+
+    userEvent.type(search, 'abcdef');
+    expect(screen.queryByText('Sua busca não retornou resultados')).toBeInTheDocument();
+  });
+
+  it('should load more posts', async () => {
+    render(<Home />);
+
+    await waitForElementToBeRemoved(
+      () => screen.queryByText('Sua busca não retornou resultados', { name: 'MyDialog' }),
+      { timeout: 99999 },
+    );
+
+    const button = screen.getByRole('button', { name: /load more posts/i });
+
+    userEvent.click(button);
+    expect(screen.getByRole('heading', { name: 'title4' })).toBeInTheDocument();
+    expect(button).toBeDisabled();
   });
 });
